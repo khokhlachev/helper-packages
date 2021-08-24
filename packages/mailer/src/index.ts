ab2c1dd5bd932bdb14f7c7f2832653deb9d3c18e
Sergey Khokhlachev <sergey@khokhlachev.com>
2021-08-24 18:02:59 +0300

    refactor: 💡 Mailer: move lib files into the root

--- packages/mailer/src/index.ts
+++ packages/mailer/src/index.ts
@@ -1,4 +1,2 @@
 export { Mailer } from "./mailer";
-export { MailgunTransport } from "./mailgun";
-export { MailchimpTransport } from "./mailchimp";
 export { validateReCaptcha } from "./validate-recaptcha";
