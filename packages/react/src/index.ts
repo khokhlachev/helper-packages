cf24ac0491869095db13e3aa40862421e8716797
Sergey Khokhlachev <sergey@khokhlachev.com>
2021-10-18 19:56:34 +0300

    feat(react): update index exports

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,6 +2,6 @@ export { RafValue } from "./raf-value";
 export { useMounted, ClientOnly } from "./use-client-only";
 export { useMeasure } from "./use-measure";
 export { useMedia, Media } from "./use-media";
-export { useBreakpoint } from "./use-breakpoint";
+export { useBreakpoint, Breakpoint, BreakpointUpTo } from "./use-breakpoint";
 export { offset, useOffset } from "./use-offset";
 export { useIsomorphicLayoutEffect } from "./use-isomorphic-layout-effect";
