fcf959c6e7cc3849250a8c1f3b8824042b539d35
Sergey Khokhlachev <sergey@khokhlachev.com>
2021-10-19 20:25:43 +0300

    feat(utils): add slugify

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -73,14 +73,14 @@ export function formatISOString(string: string, mask = "dd.MM") {
   return string.replace(
     /^(\d{2,4})-(\d{2})-(\d{2})T?(\d{2})?:?(\d{2})?.*/,
     function (_, yyyy, MM, dd, hh = "00", mm = "00") {
-      const parts: Record<string, string> = { yyyy, dd, MM, hh, mm }
-      const separator = mask.replace(/\w/g, "")[0]
+      const parts: Record<string, string> = { yyyy, dd, MM, hh, mm };
+      const separator = mask.replace(/\w/g, "")[0];
       return mask
         .split(separator)
         .map((part) => parts[part])
-        .join(separator)
+        .join(separator);
     }
-  )
+  );
 }
 
 export function range(start: number): number[];
@@ -95,6 +95,87 @@ export function range(...args: number[]): number[] {
   }
 
   return new Array(len).fill(0).map((_, i) => start + i * direction);
+}
+
+export function slugify(string: string) {
+  let str = string
+    .normalize("NFD") // split an accented letter in the base letter and the acent
+    .replace(/[\u0300-\u036f]/g, "") // remove all previously split accents
+    .toLowerCase()
+    .trim();
+
+  let from = [
+    "а",
+    "б",
+    "в",
+    "г",
+    "д",
+    "е",
+    "ж",
+    "з",
+    "и",
+    "й",
+    "к",
+    "л",
+    "м",
+    "н",
+    "о",
+    "п",
+    "р",
+    "с",
+    "т",
+    "у",
+    "ф",
+    "х",
+    "ц",
+    "ч",
+    "ш",
+    "щ",
+    "ъ",
+    "ь",
+    "ю",
+    "я",
+  ];
+  let to = [
+    "a",
+    "b",
+    "v",
+    "g",
+    "d",
+    "e",
+    "zh",
+    "z",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "r",
+    "s",
+    "t",
+    "u",
+    "f",
+    "h",
+    "c",
+    "ch",
+    "sh",
+    "sht",
+    "y",
+    "",
+    "iu",
+    "ia",
+  ];
+
+  for (let key in from) {
+    str = str.replace(new RegExp(from[key], "g"), to[key]);
+  }
+
+  return str
+    .replace(/[^a-z0-9 ]/g, "") // remove all chars not letters, numbers and spaces (to be replaced)
+    .replace(/\s+/g, "-"); // separator
 }
 
 export { createError, createLogger } from "./logger";
